9c89cd9eac98a0332ce4ee94eb5044dd86bd5a89
afurta <furtaa@mail.ru>
2022-10-26 21:48:37 +0500

    Configure initState

---
+++ src/components/MainForm/MainForm.tsx
@@ -0,0 +1,49 @@
+import {FC, useState} from 'react'
+import { authController, loginController } from '../../controllers/Auth'
+import LoginInput from '../Inputs/LoginInput'
+import PasswordInput from '../Inputs/PasswordInput'
+import './MainForm.scss'
+
+const AuthForm:FC = () => {
+    const [isAuth, setIsAuth] = useState<boolean>(false)
+
+    return (
+        <div className='form' onSubmit={(e) => e.preventDefault()}>
+            <form >
+                {
+                  isAuth
+                  ? <h1 className='form__heading'>Авторизация</h1>
+                  : <h1 className='form__heading'>Регистрация</h1>
+                }
+                <div className='form__content'>
+                    <div className='form__field'>
+                        <LoginInput className='form__field-input'/>
+                    </div>
+                    <div className='form__field'>
+                        <PasswordInput className='form__field'/>
+                    </div>
+                    { isAuth && <a href="#" className='form__link'>Забыл пароль?</a> }
+                </div>
+                <div className='form__action-block'>
+                    {
+                        !isAuth
+                            ? <button className='form__button' onClick={() => setIsAuth(!isAuth)}>Авторизация</button>
+                            : <button className='form__button' onClick={() => setIsAuth(!isAuth)}>Регистрация</button>
+                    }
+                    <button 
+                        className='form__button form__button_active'
+                        // onClick={()=>{
+                        //     isAuth 
+                        //     ? authController()
+                        //     : loginController()
+                        // }}
+                    >
+                        Войти
+                    </button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default AuthForm
